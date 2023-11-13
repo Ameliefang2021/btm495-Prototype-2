6e1214156ad6495ffd8acab7e2ffb18afb7ed856
amaomizi <amaomizi@devxb.com>
2023-11-12 22:55:37 -0500

    Add project files.

---
+++ Prototype 1/Accountant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_1
+{
+    public class Accountant : Employee
+    {
+
+        
+
+        public Accountant(int eID, string n, DateTime st, double s)
+           : base(eID, n, st, s)
+        {
+
+        }
+
+        public void ProcessPmt(Invoice invoice)
+        {
+            invoice.Balance = 0;
+            //invoice.Accountant = this;
+        }
+
+    }
+}
---
+++ Prototype 1/Client.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_1
+{
+    public class Client
+    {
+       
+        private int clientID;
+        private string firstName;
+        private string lastName;
+        private double familyIncome;
+        private string livingArrangement;
+        private int noChildren;
+        private List<Appointment> appointments;
+        private List<Application> applications;
+        private List<Invoice> invoices;
+
+        public int ClientID
+        {
+            get { return clientID; }
+            set { clientID = value; }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public double FamilyIncome
+        {
+            get { return familyIncome; }
+            set { familyIncome = value; }
+        }
+
+        public string LivingArrangement
+        {
+            get { return livingArrangement; }
+            set { livingArrangement = value; }
+        }
+
+        public int NoChildren
+        {
+            get { return noChildren; }
+            set { noChildren = value; }
+        }
+
+        public Client() { }
+
+        public Client(int cID, string fn, string ln, double fi, string la, int nc)
+        {
+            ClientID = cID;
+            FirstName = fn;
+            LastName = ln;
+            FamilyIncome = fi;
+            LivingArrangement = la;
+            NoChildren = nc;
+        }
+
+        public List<Application> applyAdoption(int appID, string stat,Client client,Animal animal)
+        {
+            Application adopotion = new Application(appID, stat, client, animal);
+            List<Application> adoptionList = new List<Application>();
+            adoptionList.Add(adopotion);
+
+            return adoptionList;
+        }
+
+        public List<Appointment> bookAppointment(int appointID, DateTime ti, Client c,Agent a)
+        {
+            Appointment app =  new Appointment(appointID, ti, c, a);
+            List<Appointment> appointList = new List<Appointment>();
+            appointList.Add(app);
+
+            return appointList;
+
+        }
+
+        public void cancalAppointment(Appointment app)
+        {
+            appointments = new List<Appointment>();
+            appointments.Remove(app);
+        }
+
+
+    }
+}
